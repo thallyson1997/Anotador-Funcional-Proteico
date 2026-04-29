d7d52dc135ab664400701324d6dfed0bd9efac47
Thallyson Gabriel <thallyson.gabriel@discente.ufma.br>
2026-04-29 15:17:11 -0300

    Atualização 44

--- main.py
+++ main.py
@@ -681,5 +681,6 @@ if __name__ == "__main__":
         host="127.0.0.1",
         port=8000,
         log_level="info",
-        reload=True
+        reload=True,
+        reload_excludes=["**/__pycache__/**", "**/*.pyc", "**/debug_proteins.txt"]
     )
